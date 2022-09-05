28512aecec6cf958ab81af151883f96c60493b2e
Andres2908 <saavedratrejo24@gmail.com>
2022-09-05 00:06:11 -0500

    listo

--- src/components/Botones.js
+++ src/components/Botones.js
@@ -1,29 +1,63 @@
 import React, {useState} from "react";
+import axios from 'axios'
 
 export function Botones() {
     const [count, setCount] = useState(0)
-    const addCount = () => {
-        setCount(count+1)
+    const [countColor, setCountColor] = useState({
+        marginLeft: 300,
+        marginRight: 300,
+        fontSize: 40,
+        background: "red",
+    })
+    const [data, setData] = useState({
+        numero: 0, 
+        tipo: 0
+    })
+    const request = (numero, tipo) => {
+        axios.put('http://localhost:8080/operation', {numero, 
+        tipo})
+        .then((res) => {
+            setCount(res.data.count)
+            setCountColor({...countColor, background: res.data.countColor})
+        }) 
+        .catch((err) => {
+            console.log(err)
+        })
     }
-    const substracCount = () => {
-        setCount(count-2)
+
+    const add = () => {
+        request(count, 0)
     }
-    const divideCount = () => {
-        setCount(count/3)
+
+    const substract = () => {
+        request(count, 1)
     }
-    const resetCount = () => {
-        setCount(0)
+
+    const divide = () => {
+        request(count, 2)
+    }
+
+    const reset = () => {
+        request(count, 3)
     }
 
     let center = {textAlign: "center", paddingTop: 40} //css
-    
-    return(<div style={center}>
-            <button onClick={addCount}>Add</button>
-            <button onClick={substracCount}>Substrac</button>
-            <button onClick={divideCount}>Divide</button>
-            <button onClick={resetCount}>Reset</button>
+    let button = {margin: 4,
+        backgroundColor: "black",
+        color: "white",
+        padding: 8,
+        borderRadius: 8,
+        textTransform: "uppercase",
+        paddingLeft: 32,
+        paddingRight: 32}
 
-            <p style={{fontSize: 40}}>{count}</p>
+    return(<div style={center}>
+            <button style={button} onClick={add}>Add</button>
+            <button style={button} onClick={substract}>Substrac</button>
+            <button style={button} onClick={divide}>Divide</button>
+            <button style={button} onClick={reset}>Reset</button>
+
+            <p style={countColor}>{count}</p>
 
         </div>
     )
